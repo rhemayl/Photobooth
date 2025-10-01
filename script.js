efd417c0c2287735fe2b49c75da24bb4b0d7ce75
rhemayl <rhema9735@gmail.com>
2025-10-01 20:25:01 +0100

    timer and added photostrip with 4 images

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const video = document.getElementById("video");
 const snap = document.getElementById("snap")
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d")
+const ctx = canvas.getContext("2d");
+const countdown = document.getElementById("countdown")
+let slot = 1;
 
 
 navigator.mediaDevices.getUserMedia({video:true})
@@ -12,37 +14,46 @@ navigator.mediaDevices.getUserMedia({video:true})
         console.log("Error:" +err);
     })
 
+
+
 function chooseBooth(templateName) {
     currentBooth = templateName;
     video.style.filter = boothFilters(templateName);
 }
 
 snap.addEventListener("click", () => {
+    let slot = 1;
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight * 4;
-
-    let count = 3
-    const countdown = setInterval(() =>{
-        console.log(count);
-        count--;
-        if (count<0) {
-            clearInterval(countdown);
-        }
-    },1000)
-
-    ctx.save();
-    ctx.scale(-1,1);
-    ctx.drawImage(video, -canvas.width, 0, canvas.width, canvas.height)
-    ctx.restore()
-
+    takePhoto()
     if(templateName) {
         const templateImage = new Image();
         templateImage.src = "template/" + templateName;
-        templateImage.Onload = () =>{
+        templateImage.onload = () =>{
             ctx.drawImage(templateImage, 0, 0, canvas.width, canvas.height);
         }
     }
     })
 
 
+function takePhoto(){
+    if (slot>4) return;
+    let count = 3;
 
+    countdown.textContent = count
+
+    const countdowntimer = setInterval(() =>{
+        countdown.textContent = count;
+        count--;
+        if (count<0) {
+            ctx.save();
+            ctx.scale(-1,1);
+            ctx.drawImage(video, -canvas.width, (slot-1) * video.videoHeight, canvas.width, video.videoHeight)
+            ctx.restore()
+            clearInterval(countdowntimer);
+            countdown.textContent = ""
+            slot++;
+            takePhoto()
+        }
+    },1000)
+}
